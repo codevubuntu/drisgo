ad14ed70313b0d62e13909587aec2ee6a5be60b1
codevubuntu <codev.ubuntu@gmail.com>
2019-06-16 23:21:47 +0100

    Update main.go

--- main.go
+++ main.go
@@ -4,30 +4,18 @@ import (
   "log"
   "fmt"
   "net/http"
+  "html/template"
   "os"
 )
 
-func determineListenAddress() (string, error) {
-  port := os.Getenv("PORT")
-  if port == "" {
-    return "", fmt.Errorf("$PORT not set")
-  }
-  return ":" + port, nil
-}
 
 func hello(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintln(w, "Hello World")
+  t, _ := template.ParseFiles("index.html")
+  t.Execute(w, nil)
 }
 
 func main() {
-  addr, err := determineListenAddress()
-  if err != nil {
-    log.Fatal(err)
-  }
-
+  port := os.Getenv("PORT")
   http.HandleFunc("/", hello)
-  log.Printf("Listening on %s...\n", addr)
-  if err := http.ListenAndServe(addr, nil); err != nil {
-    panic(err)
-  }
+  log.Fatal(http.ListenAndServe(":"+port, nil))
 }
