5b666004a0e47db287be19e9d600d261dcc63e42
codevubuntu <codev.ubuntu@gmail.com>
2019-06-19 18:14:44 +0100

    Update main.go

--- main.go
+++ main.go
@@ -38,8 +38,9 @@ func checkErr(err error) {
 }
 
 func connexion() *sql.DB {
-	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", DB_USER, DB_PASSWORD, DB_NAME)
-	db, err := sql.Open("postgres", dbinfo)
+	//dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", DB_USER, DB_PASSWORD, DB_NAME)
+	dbUrl := os.Getenv("DATABASE_URL")
+	db, err := sql.Open("postgres", dbUrl)
 	checkErr(err)
 	return db
 }
